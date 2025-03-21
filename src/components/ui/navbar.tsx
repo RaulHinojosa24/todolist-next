898bacfd3e4a67e3bf82f1bacdc7ae7d529f0a2f
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2025-03-21 17:03:05 +0100

    add logo component to navbar for improved branding

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -2,11 +2,13 @@ import { Button } from "./button"
 import Link from "next/link"
 import UserActions from "../navbar/user-actions"
 import { ThemeToggle } from "./theme-toggle"
+import Logo from "../assets/Logo"
 
 export default function NavBar() {
   return (
     <nav className="flex items-center justify-between gap-4 p-2">
       <div className="flex gap-2">
+        <Logo className="h-9" />
         <Link href={"/"}>
           <Button variant={"link"}>Home</Button>
         </Link>
