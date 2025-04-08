481c2e9b70a54f53856c3313c19a20a874472104
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2025-04-08 13:18:47 +0200

    fix: refactor LoginForm to manage username and password state for improved functionality

--- src/components/login/form.tsx
+++ src/components/login/form.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import { useActionState } from "react"
+import { useActionState, useState } from "react"
 import { authenticate } from "@/lib/actions"
 import { useSearchParams } from "next/navigation"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import PasswordInput from "../ui/password-input"
 
 export default function LoginForm() {
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get("callbackUrl") || "/tasks"
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined
@@ -19,16 +22,28 @@ export default function LoginForm() {
     <form action={formAction} className="space-y-3">
       <div>
         <Label htmlFor="username">Username</Label>
-        <Input name="username" required></Input>
+        <Input
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        ></Input>
       </div>
       <div>
         <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" required></Input>
+        <PasswordInput
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
       </div>
       <input type="hidden" name="redirectTo" value={callbackUrl} />
       {errorMessage && (
         <>
-          <p className="text-sm text-red-500"> {errorMessage} </p>
+          <p className="text-sm text-destructive"> {errorMessage} </p>
         </>
       )}
       <Button type="submit" disabled={isPending}>
